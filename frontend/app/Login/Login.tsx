ca797a61a0f73db74ac59c7924bdab134f4471e7
Marco <formante.marco@gmail.com>
2026-01-23 20:57:39 +0000

    create login page with form component

--- frontend/app/Login/Login.tsx
+++ frontend/app/Login/Login.tsx
@@ -1,10 +1,11 @@
 import { Form } from "~/components/loginForm/Form";
+import LoginPhoto from "~/assets/images/loginPhoto.jpeg"
 
 export function LoginPage() {
 
   return (
-    <main>
-      <div className="ml-[150px] p-[40px] w-[398px] bg-white rounded-2xl mt-[151px]">
+    <main className="max-[1440px]:flex  justify-evenly items-end min-[1440px]:mt-[150px] gap-[20px] ">
+      <div className="ml-[150px] p-[40px] w-[398px] bg-white rounded-2xl  max-[1400px]:ml-[2vw]">
           <h1 className="loginTitle">
             Transformez <br/>vos stats en résultats
           </h1>
@@ -13,6 +14,15 @@ export function LoginPage() {
             <Form/>
           </div>
           <p className="text-[14px] font-normal black-text mb-[40px]">Mot de passe oublié ?</p>
+      </div>
+      <div className="relative h-full max-[1440px]:pt-[40px]">
+          <img className="min-[1440px]:fixed top-0 right-0 max-[1440px]:block  min-w-[468px]  max-[1440px]:w-[358px] " src={LoginPhoto} alt="" />
+          <div className="absolute right-0 bottom-[0px]  max-[1440px]:bottom-2.5  max-[1440px]:right-2.5 w-[288px] bg-[#FFFFFF] rounded-[50px] flex justify-center items-center p-[16px] ">
+            <p className="text-[12px] text-[#0B23F4]">
+              Analysez vos performances en un clin d’œil,
+              suivez vos progrès et atteignez vos objectifs.
+            </p>
+          </div>
       </div>
     </main>
   )
