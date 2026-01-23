cfbff981d1b9f267ecf13b119039144cf5dd94cb
Marco <formante.marco@gmail.com>
2026-01-23 21:02:48 +0000

    create loading component

---
+++ frontend/app/components/loading/Loading.tsx
@@ -0,0 +1,9 @@
+export function Loading(){
+    return (
+         <div className="h-full w-full bg-[#f2f3ff] flex justify-center items-center">
+            <div>
+                <div className={`w-[80px] h-[80px] rounded-full border-4 border-t-0 border-l-0 border-[#0B23F4] animate-spin`}></div>
+            </div>
+        </div>
+    )
+}
