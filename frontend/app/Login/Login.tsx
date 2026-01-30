7b06bb0c215c0f520881f5845af082c1fef6c1f9
Marco <formante.marco@gmail.com>
2026-01-30 12:52:52 +0000

    change imgae position, add alt text

--- frontend/app/Login/Login.tsx
+++ frontend/app/Login/Login.tsx
@@ -16,7 +16,7 @@ export function LoginPage() {
           <p className="text-[14px] font-normal black-text mb-[40px]">Mot de passe oublié ?</p>
       </div>
       <div className="relative h-full max-[1440px]:pt-[40px]">
-          <img className="min-[1440px]:fixed top-0 right-0 max-[1440px]:block  min-w-[468px]  max-[1440px]:w-[358px] " src={LoginPhoto} alt="" />
+          <img className="min-[1440px]:fixed top-0 right-0 min-[1440px]:right-40 max-[1440px]:block  min-w-[468px]  max-[1440px]:w-[358px] " src={LoginPhoto} alt="SportSee" />
           <div className="absolute right-0 bottom-[0px]  max-[1440px]:bottom-2.5  max-[1440px]:right-2.5 w-[288px] bg-[#FFFFFF] rounded-[50px] flex justify-center items-center p-[16px] ">
             <p className="text-[12px] text-[#0B23F4]">
               Analysez vos performances en un clin d’œil,
