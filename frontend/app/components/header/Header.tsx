70eb28e270657b6eab80d7e728a264bb0b539ff0
Marco <formante.marco@gmail.com>
2026-01-30 12:53:18 +0000

    refactoring

--- frontend/app/components/header/Header.tsx
+++ frontend/app/components/header/Header.tsx
@@ -1,22 +1,19 @@
-import { Navigate, NavLink, useLoaderData, useLocation, useNavigate } from "react-router";
+import {  NavLink }  from "react-router";
 import Logo from "../../assets/images/Logo.png";
-import { useFetch } from "~/hooks/useFetch";
-import { useContext, useEffect, useState } from "react";
-import { initialData, sessionExist, UserDataContext } from "~/providers/userProvider";
-import { useLogout } from "~/hooks/useLogout";
+import { sessionExist,  } from "~/providers/userProvider";
+
 import { AdminHeader } from "./AdminHeader";
 
 
 
 export function Header(){
-    const token = sessionExist && sessionStorage.getItem("toksen")
 
     return (
-         <header className="mt-[35px] pl-[150px] pr-[150px] h-[49px] max-[1300px]:px-0">
+         <header className="mt-[35px] max-w-[1140px] m-auto h-[49px] max-[1300px]:px-0">
             <nav className="flex justify-between items-center h-full">
                 <div>
                     <NavLink to={"/profile"}>
-                        <img src={Logo} alt="" />
+                        <img src={Logo} alt="SportSee" />
                     </NavLink>
                 </div>
                 <AdminHeader/>
